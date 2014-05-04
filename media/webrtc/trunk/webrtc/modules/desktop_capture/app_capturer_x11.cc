4d5eac704c6e88c775af6cf9e48e2b9a59fd783c
vagouzhou <vagouzhou@gmail.com>
2014-05-04 09:29:00 +0800

    make compile success on linux

--- media/webrtc/trunk/webrtc/modules/desktop_capture/app_capturer_x11.cc
+++ media/webrtc/trunk/webrtc/modules/desktop_capture/app_capturer_x11.cc
@@ -32,110 +32,31 @@ namespace webrtc {
 
 	namespace {
 
-		// Convenience wrapper for XGetWindowProperty() results.
-		template <class PropertyType>
-		class XWindowProperty {
-		public:
-			XWindowProperty(Display* display, Window window, Atom property)
-				: is_valid_(false),
-				size_(0),
-				data_(NULL) {
-					const int kBitsPerByte = 8;
-					Atom actual_type;
-					int actual_format;
-					unsigned long bytes_after;  // NOLINT: type required by XGetWindowProperty
-					int status = XGetWindowProperty(display, window, property, 0L, ~0L, False,
-						AnyPropertyType, &actual_type,
-						&actual_format, &size_,
-						&bytes_after, &data_);
-					if (status != Success) {
-						data_ = NULL;
-						return;
-					}
-					if (sizeof(PropertyType) * kBitsPerByte != actual_format) {
-						size_ = 0;
-						return;
-					}
 
-					is_valid_ = true;
-			}
-
-			~XWindowProperty() {
-				if (data_)
-					XFree(data_);
-			}
-
-			// True if we got properly value successfully.
-			bool is_valid() const { return is_valid_; }
-
-			// Size and value of the property.
-			size_t size() const { return size_; }
-			const PropertyType* data() const {
-				return reinterpret_cast<PropertyType*>(data_);
-			}
-			PropertyType* data() {
-				return reinterpret_cast<PropertyType*>(data_);
-			}
-
-		private:
-			bool is_valid_;
-			unsigned long size_;  // NOLINT: type required by XGetWindowProperty
-			unsigned char* data_;
-
-			DISALLOW_COPY_AND_ASSIGN(XWindowProperty);
-		};
-
-		class AppCapturerLinux : public AppCapturer,
-			public SharedXDisplay::XEventHandler {
+		class AppCapturerLinux : public AppCapturer{
 		public:
 			AppCapturerLinux(const DesktopCaptureOptions& options);
 			virtual ~AppCapturerLinux();
 
 			// AppCapturer interface.
-			virtual bool GetWindowList(WindowList* windows) OVERRIDE;
-			virtual bool SelectWindow(ProcessId id) OVERRIDE;
-			virtual bool BringSelectedWindowToFront() OVERRIDE;
+			virtual bool GetAppList(AppList* apps) OVERRIDE;
+			virtual bool SelectApp(ProcessId id) OVERRIDE;
+			virtual bool BringAppToFront() OVERRIDE;
 
 			// DesktopCapturer interface.
 			virtual void Start(Callback* callback) OVERRIDE;
 			virtual void Capture(const DesktopRegion& region) OVERRIDE;
 
-			// SharedXDisplay::XEventHandler interface.
-			virtual bool HandleXEvent(const XEvent& event) OVERRIDE;
 
 		private:
-			Display* display() { return x_display_->display(); }
-
-			// Iterates through |window| hierarchy to find first visible window, i.e. one
-			// that has WM_STATE property set to NormalState.
-			// See http://tronche.com/gui/x/icccm/sec-4.html#s-4.1.3.1 .
-			::Window GetApplicationWindow(::Window window);
-
-			// Returns true if the |window| is a desktop element.
-			bool IsDesktopElement(::Window window);
-
-			// Returns window title for the specified X |window|.
-			bool GetWindowTitle(::Window window, std::string* title);
-
 			Callback* callback_;
-
-			scoped_refptr<SharedXDisplay> x_display_;
-
-			Atom wm_state_atom_;
-			Atom window_type_atom_;
-			Atom normal_window_type_atom_;
-			bool has_composite_extension_;
-
-			::Window selected_process_;
-			XServerPixelBuffer x_server_pixel_buffer_;
+			ProcessId selected_process_;
 
 			DISALLOW_COPY_AND_ASSIGN(AppCapturerLinux);
 		};
 
 		AppCapturerLinux::AppCapturerLinux(const DesktopCaptureOptions& options)
 			: callback_(NULL),
-			x_display_(options.x_display()),
-			has_composite_extension_(false),
 			selected_process_(0) {
 		}
 
@@ -143,13 +64,13 @@ namespace webrtc {
 		}
 
 		// AppCapturer interface.
-		virtual bool GetWindowList(WindowList* windows){
+		bool AppCapturerLinux::GetAppList(AppList* apps){
 			return true;
 		}
-		virtual bool SelectWindow(ProcessId id){
+		bool AppCapturerLinux::SelectApp(ProcessId id){
 			return true;
 		}
-		virtual bool BringSelectedWindowToFront(){
+		bool AppCapturerLinux::BringAppToFront(){
 			return true;
 		}
 
@@ -168,8 +89,8 @@ namespace webrtc {
 
 	// static
 	AppCapturer* AppCapturer::Create(const DesktopCaptureOptions& options) {
-		if (!options.x_display())
-			return NULL;
+	//	if (!options.x_display())
+	//		return NULL;
 		return new AppCapturerLinux(options);
 	}
 
