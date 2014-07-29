13ed4b20836ebf74254117785e8d05c43ba56972
vagouzhou <vagouzhou@gmail.com>
2014-07-29 19:00:39 +0800

    list application and update capture on x11

--- media/webrtc/trunk/webrtc/modules/desktop_capture/app_capturer_x11.cc
+++ media/webrtc/trunk/webrtc/modules/desktop_capture/app_capturer_x11.cc
@@ -27,13 +27,15 @@
 #include "webrtc/system_wrappers/interface/logging.h"
 #include "webrtc/system_wrappers/interface/scoped_ptr.h"
 #include "webrtc/system_wrappers/interface/scoped_refptr.h"
+#include "webrtc/modules/desktop_capture/x11/shared_x_util.h"
 
 namespace webrtc {
 
 	namespace {
 
 
-		class AppCapturerLinux : public AppCapturer{
+		class AppCapturerLinux : public AppCapturer
+									,public SharedXDisplay::XEventHandler{
 		public:
 			AppCapturerLinux(const DesktopCaptureOptions& options);
 			virtual ~AppCapturerLinux();
@@ -47,27 +49,63 @@ namespace webrtc {
 			virtual void Start(Callback* callback) OVERRIDE;
 			virtual void Capture(const DesktopRegion& region) OVERRIDE;
 
+			// SharedXDisplay::XEventHandler interface.
+			virtual bool HandleXEvent(const XEvent& event) OVERRIDE;
+
+		protected:
+			Display* display() { return x_display_->display(); }
+			bool initXServerPixBuffer();
+			::Window getCurrentRootWindow();
+			void captureWebRTC(const DesktopRegion& region);
+			void captureSample(const DesktopRegion& region);
 
 		private:
 			Callback* callback_;
 			ProcessId selected_process_;
+			XServerPixelBuffer x_server_pixel_buffer_;
 
+			scoped_refptr<SharedXDisplay> x_display_;
+			bool has_composite_extension_;
+
+			::Window root_window_;
 			DISALLOW_COPY_AND_ASSIGN(AppCapturerLinux);
 		};
 
 		AppCapturerLinux::AppCapturerLinux(const DesktopCaptureOptions& options)
 			: callback_(NULL),
-			selected_process_(0) {
+		      x_display_(options.x_display()),
+		      root_window_(DefaultRootWindow(display())),
+		      selected_process_(0) {
+			int event_base, error_base, major_version, minor_version;
+			  if (XCompositeQueryExtension(display(), &event_base, &error_base) &&
+			      XCompositeQueryVersion(display(), &major_version, &minor_version) &&
+			      // XCompositeNameWindowPixmap() requires version 0.2
+			      (major_version > 0 || minor_version >= 2)) {
+			    has_composite_extension_ = true;
+			  } else {
+			    LOG(LS_INFO) << "Xcomposite extension not available or too old.";
+			  }
+
+			  x_display_->AddEventHandler(ConfigureNotify, this);
 		}
 
 		AppCapturerLinux::~AppCapturerLinux() {
+			x_server_pixel_buffer_.Release();
+			x_display_->RemoveEventHandler(ConfigureNotify, this);
 		}
 
 		// AppCapturer interface.
 		bool AppCapturerLinux::GetAppList(AppList* apps){
 			return true;
 		}
-		bool AppCapturerLinux::SelectApp(ProcessId id){
+		bool AppCapturerLinux::SelectApp(ProcessId processId){
+			selected_process_ = processId;
+			getCurrentRootWindow();
+			if (!initXServerPixBuffer()){
+				selected_process_ = 0;
+				return false;
+			}
+
 			return true;
 		}
 		bool AppCapturerLinux::BringAppToFront(){
@@ -82,7 +120,104 @@ namespace webrtc {
 			callback_ = callback;
 		}
 		void AppCapturerLinux::Capture(const DesktopRegion& region) {
-			//callback_->OnCaptureCompleted(frame);
+			return captureWebRTC(region);
+		}
+		void AppCapturerLinux::captureWebRTC(const DesktopRegion& region) {
+			x_display_->ProcessPendingXEvents();
+
+			  if (!has_composite_extension_) {
+			    // Without the Xcomposite extension we capture when the whole window is
+			    // visible on screen and not covered by any other window. This is not
+			    // something we want so instead, just bail out.
+			    LOG(LS_INFO) << "No Xcomposite extension detected.";
+			    callback_->OnCaptureCompleted(NULL);
+			    return;
+			  }
+
+			  DesktopFrame* frame =
+			      new BasicDesktopFrame(x_server_pixel_buffer_.window_size());
+
+			  x_server_pixel_buffer_.Synchronize();
+			  x_server_pixel_buffer_.CaptureRect(DesktopRect::MakeSize(frame->size()),
+			                                     frame);
+
+			  callback_->OnCaptureCompleted(frame);
+		}
+
+		void AppCapturerLinux::captureSample(const DesktopRegion& region) {
+			//Capture screen
+
+			//calculate app visual/foreground region
+
+			//fill background
+
+			//fill visual
+
+			//fill foreground
+
+			//event
+		}
+
+		bool AppCapturerLinux::HandleXEvent(const XEvent& event) {
+		  if (event.type == ConfigureNotify) {
+		    XConfigureEvent xce = event.xconfigure;
+		    if (!DesktopSize(xce.width, xce.height).equals(
+		            x_server_pixel_buffer_.window_size())) {
+		    	initXServerPixBuffer();
+		    	return true;
+		    }
+		  }
+		  return false;
+		}
+
+		bool AppCapturerLinux::initXServerPixBuffer(){
+			if (!x_server_pixel_buffer_.Init(display(), root_window_)) {
+				LOG(LS_ERROR) << "Failed to initialize pixel buffer after resizing.";
+				return false;
+			}
+
+			return true;
+		}
+
+		::Window AppCapturerLinux::getCurrentRootWindow(){
+			::Window window;
+
+			WindowUtilX11 window_util_x11(x_display_);
+			int num_screens = XScreenCount(display());
+			for (int screen = 0; screen < num_screens; ++screen) {
+				::Window root_window = XRootWindow(display(), screen);
+				::Window parent;
+				::Window *children;
+				unsigned int num_children;
+				int status = XQueryTree(display(), root_window, &root_window, &parent,
+										&children, &num_children);
+				if (status == 0) {
+					LOG(LS_ERROR) << "Failed to query for child windows for screen "
+					<< screen;
+					continue;
+				}
+
+				for (unsigned int i = 0; i < num_children; ++i) {
+					// Iterate in reverse order to return windows from front to back.
+					::Window app_window =children[num_children - 1 - i];//window_util_x11.GetApplicationWindow(children[num_children - 1 - i]);
+
+					if (!app_window
+						|| window_util_x11.IsDesktopElement(app_window)
+						|| window_util_x11.GetWindowStatus(app_window) == WithdrawnState )
+					continue;
+
+					unsigned int processId = window_util_x11.GetWindowProcessID(app_window);
+					if(processId!=0 && processId==selected_process_){
+						window = app_window;
+						break;
+					}
+				}
+
+				if (children)
+					XFree(children);
+			}
+			root_window_ = DefaultRootWindow(display());
+			return window;
 		}
 
 	}  // namespace
