77e0af70725567645d30bcb999ad9af4481b86cd
vagouzhou <vagouzhou@gmail.com>
2014-08-10 17:32:04 +0800

    fix building fail on Mac Platform

--- media/webrtc/trunk/webrtc/modules/desktop_capture/desktop_device_info.cc
+++ media/webrtc/trunk/webrtc/modules/desktop_capture/desktop_device_info.cc
@@ -1,6 +1,7 @@
 * License, v. 2.0. If a copy of the MPL was not distributed with this file,
 * You can obtain one at http://mozilla.org/MPL/2.0/. */
 #include "webrtc/modules/desktop_capture/desktop_device_info.h"
+#include <sstream>
 
 namespace webrtc{
     
